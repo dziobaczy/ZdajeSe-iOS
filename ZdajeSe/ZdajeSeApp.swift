0954fb190614219cfe6d6b0265889daa0e4ef9fc
OfftheCode <offthecode@gmail.com>
2020-10-31 17:14:50 +0100

    Initial Commit

---
+++ ZdajeSe/ZdajeSeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZdajeSeApp.swift
+//  ZdajeSe
+//
+//  Created by Piotr Szadkowski on 31/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ZdajeSeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
