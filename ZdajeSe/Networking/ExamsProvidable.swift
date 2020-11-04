550f093add1b65152eef6285c7e3b1a7c5b9949b
OfftheCode <offthecode@gmail.com>
2020-11-04 19:50:23 +0100

    Add Exams providable requierments

--- ZdajeSe/Networking/ExamsProvidable.swift
+++ ZdajeSe/Networking/ExamsProvidable.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import Combine
+
+enum ZSError: Error, LocalizedError {
+    case networkError(message: String)
+    
+    var errorDescription: String? {
+        switch self {
+        case .networkError(let message):
+            return message
+        }
+    }
+}
+
+protocol ExamsProvidable: class {
+    func getExams() -> AnyPublisher<[ExamOverview], ZSError>
+    func getQuestions(forExamId exam: String) -> AnyPublisher<[Exam], ZSError>
+}
