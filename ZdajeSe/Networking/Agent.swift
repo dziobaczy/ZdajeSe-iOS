afd652687eff157cd9bb288ebc599934a66753a4
OfftheCode <offthecode@gmail.com>
2020-11-04 09:14:39 +0100

    Add network agent

---
+++ ZdajeSe/Networking/Agent.swift
@@ -0,0 +1,35 @@
+//
+//  Agent.swift
+//  ZdajeSe
+//
+//  Created by Piotr Szadkowski on 04/11/2020.
+//
+
+import Foundation
+import Combine
+
+struct Agent {
+    
+    private let urlSession: URLSession
+    
+    struct Response<T: Decodable> {
+        let value: T
+        let response: URLResponse
+    }
+    
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    func run<T: Decodable>(_ request: URLRequest, _ decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<Response<T>, Error> {
+        return urlSession
+            .dataTaskPublisher(for: request)
+            .tryMap { result -> Response<T> in
+                let value = try decoder.decode(T.self, from: result.data)
+                return Response(value: value, response: result.response)
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+}
